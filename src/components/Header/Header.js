0eff88d97ec983938c97bfdc99662ebcc8e2a463
bibekggurung <bibek.grng@gmail.com>
2020-01-30 15:52:42 -0500

    Removed superfluous button Show a Ride button has been removed from Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,6 @@ const authenticatedOptions = (
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
     <Nav.Link href="#rides">Rides</Nav.Link>
-    <Nav.Link href="#rides/:id">Show A Ride</Nav.Link>
     <Nav.Link href="#create-ride">Create A Ride</Nav.Link>
   </Fragment>
 )
